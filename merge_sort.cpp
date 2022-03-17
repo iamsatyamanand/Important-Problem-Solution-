23cd56f29963f3c37e25a0d1df9f4cde1d242f89
iamsatyamanand <93544923+iamsatyamanand@users.noreply.github.com>
2022-03-17 19:57:13 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -8,23 +8,23 @@ using namespace std;
 void merge(vector<int> &num, int low, int mid, int high){
 	int left_size = mid-low+1;
 	int right_size = high-mid;
-	vector<int> left_arr(left_size);
-	vector<int> right_arr(right_size);
+	vector<int> left_num(left_size);
+	vector<int> right_num(right_size);
 	for(int i=0; i<left_size; i++){
-		left_arr[i] = num[i+low];
+		left_num[i] = num[i+low];
 	}
 	for(int i=0; i<right_size; i++){
-		right_arr[i] = num[i+mid+1];
+		right_num[i] = num[i+mid+1];
 	}
 	int left_idx = 0, right_idx = 0;
-	left_arr.push_back(INT_MAX);
-	right_arr.push_back(INT_MAX);
+	left_num.push_back(INT_MAX);
+	right_num.push_back(INT_MAX);
 	for(int i=low; i<=high; i++){
-		if(left_arr[left_idx] <= right_arr[right_idx]){
-			num[i] = left_arr[left_idx++];
+		if(left_num[left_idx] <= right_num[right_idx]){
+			num[i] = left_num[left_idx++];
 		}
 		else{
-			num[i] = right_arr[right_idx++];
+			num[i] = right_num[right_idx++];
 		}
 	}
 }
@@ -38,21 +38,15 @@ void merge_sort(vector<int> &num, int low, int high){
 }
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int t;
-	cin >> t;
-	while(t--){
-		int n;
-		cin >> n;
-		vector<int> num(n);
-		for(auto &x : num){
-			cin >> x;
-		}
-		merge_sort(num, 0, n-1);
-		for(auto &x : num){
-			cout << x << " ";
-		}
+	int n;
+	cin >> n;
+	vector<int> num(n);
+	for(auto &x : num){
+		cin >> x;
+	}
+	merge_sort(num, 0, n-1);
+	for(auto &x : num){
+		cout << x << " ";
 	}
 	return 0;
 }
